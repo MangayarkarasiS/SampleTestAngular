cab334473e657a3b6ee784dd94d570da3f6d5487
Trainer Mangai <mangai2000@gmail.com>
2024-03-16 09:14:48 +0530

    Adding root component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,10 +23,11 @@ describe('AppComponent', () => {
   it(`should have as title 'ang-test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ang-test');
+    expect(app.title).toEqual('ang-test123');
   });
 
-  it('should render title', () => {
+  it('should render title', () => {   
+
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
